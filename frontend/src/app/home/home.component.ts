3433c9009f1d4cd3849de5ebab3f39e745b8472b
Apichaya <apichayapg@gmail.com>
2018-07-16 19:03:25 +0700

    Edit review product card

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  videoList = [
+    {
+     name: 'item 1',
+     slug: 'item-1',
+      embed: '<div style="position: relative; height: 0; padding-bottom: 56.25%"><iframe width="560" height="315" src="https://www.youtube.com/embed/IMEE1_zhfhs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+    },
+    {
+      name: 'item 2',
+      slug: 'item-2',
+      embed: '<div style="position: relative; height: 0; padding-bottom: 56.25%"><iframe width="560" height="315" src="https://www.youtube.com/embed/IMEE1_zhfhs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+    },
+    {
+      name: 'item 3',
+      slug: 'item-3',
+      embed: '<div style="position: relative; height: 0; padding-bottom: 56.25%"><iframe width="560" height="315" src="https://www.youtube.com/embed/IMEE1_zhfhs" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
